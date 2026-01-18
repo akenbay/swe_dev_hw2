de205f5b438422f00b7645fb34590004d5a0114c
Aibar <aibarkenbay@gmail.com>
2026-01-18 13:48:03 +0500

    Feat: add users table

--- sql/create.sql
+++ sql/create.sql
@@ -18,6 +18,16 @@ CREATE TABLE students (
     group_id INT REFERENCES groups(id)
 );
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 CREATE TABLE schedule (
     id SERIAL PRIMARY KEY,
     faculty_id INT REFERENCES faculties(id),
